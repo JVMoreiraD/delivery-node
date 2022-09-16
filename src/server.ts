c1d9ad5077e1fc14e0dcd105be5d41fb606da2e8
jvmoreirad <jvitormoreirad@hotmail.com>
2022-09-16 13:41:36 -0300

    feat: error handle

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import "dotenv/config"
-import express from "express"
+import "express-async-errors"
+import express, { NextFunction, Request, Response } from "express"
 import { routes } from "./routes";
 
 const app = express();
 app.use(express.json())
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof Error) {
+        return response.status(400).json({
+            message: error.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "internal server error"
+    })
+})
 
 app.use(routes)
 
